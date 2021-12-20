54a46277ad47a0b77cbdf0c42ace41af7075403d
Rohith PR <praroh2@gmail.com>
2021-12-20 21:42:07 +0530

    Return newly created account

--- service/account_service.py
+++ service/account_service.py
@@ -3,3 +3,4 @@ from db.models import Account
 def create_account(uid):
     account = Account(uid=uid)
     account.save()
+    return account
--- tests/integration/test_account_service.py
+++ tests/integration/test_account_service.py
@@ -2,13 +2,14 @@ import pytest
 from service import account_service
 from db import models
 from db.models import Account
+import uuid
 
 
 def test_create_account():
-    test_account = "test-account"
-    import pudb; pudb.set_trace()
+    test_account = str(uuid.uuid4())
     with pytest.raises(Account.DoesNotExist):
         account = models.Account.get(test_account)
-    account_service.create_account(test_account)
+    created_account = account_service.create_account(test_account)
     account = models.Account.get(test_account)
+    assert created_account.uid == account.uid
     assert account.uid == test_account
